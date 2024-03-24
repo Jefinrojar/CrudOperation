e1afb4319fadaf1f7985c308ffad1e89acdae8d7
Jefinrojar <jefinrojar@gmail.com>
2024-03-24 13:12:42 +0530

    first commit

---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    
+        <nav className='nav'>
+            <h2>CRUD OPERATION</h2>
+        </nav>
+    
+  )
+}
+
+export default Header
